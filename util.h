84a87e2712e88d66c79a3150e5caf579aeca3be4
JamieABall <jamiegamer93@att.net>
2016-08-07 16:21:10 -0700

    Create util.h

---
+++ util.h
@@ -0,0 +1,15 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include <cstddef>
+#include <vector>
+#include <cstddef>
+#include "conio.h"
+using namespace std;
+
+string& deep_trim(string& target);
+vector<string>& find_paren_exper(string& target, vector<string>& expers);
+#endif
